8fd9942b36425dc6444f408908e7ac621076ffd6
Your Name <you@example.com>
2025-09-15 12:39:00 +0530

    api added

--- frontendd/src/api.jsx
+++ frontendd/src/api.jsx
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'https://task-manager-app-202i.onrender.com' ;
 
 
 async function request(path, options = {}){
